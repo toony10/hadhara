975c0a293da2b0574546a9c6685a453828436c7c
toony10 <amiranton257@gmail.com>
2025-02-23 01:58:16 +0200

    politics/army

--- app/commponents/politics/MilitaryWars.tsx
+++ app/commponents/politics/MilitaryWars.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import TopicHeader from '../custome/TopicHeader';
+import ContentBlock from '../custome/ContentBlock';
+// import ReferencesSection from '../custome/ReferencesSection';
+
+interface ContentData {
+    title: string;
+    description?: string;
+    listItems?: { title: string; description?: string }[];
+    imageUrls?: string[];
+    imageDescriptions?: string[];
+}
 
 function MilitaryWars() {
+    const [data, setData] = useState<ContentData[]>([]);
+
+
+    useEffect(() => {
+        fetch("/data/Politics/MilitaryWars.json")
+            .then((res) => res.json())
+            .then((jsonData) => setData(jsonData));
+    }, []);
+
     return (
-        <div>MilitaryWars</div>
+        <div>
+            <TopicHeader
+                title="الجيش والحروب"
+                description="لعب الجيش المصري دورًا محوريًا في تاريخ مصر القديمة، حيث لم يكن مجرد قوة عسكرية، بل كان عاملًا رئيسيًا في تحقيق الاستقرار والتوسع الإقليمي. قاد الفراعنة الجيوش في معارك دفاعية وهجومية، مستخدمين أسلحة متطورة، وعجلات حربية، واستراتيجيات قتالية متقدمة. ساهمت الحروب في تعزيز قوة مصر السياسية والعسكرية، مما جعلها واحدة من أعظم الحضارات في العالم القديم."
+            />
+            <div>
+                { data.map((item, index) => (
+                    <ContentBlock
+                        key={ index }
+                        title={ item.title }
+                        description={ item.description }
+                        listItems={ item.listItems }
+                        imageUrls={ item.imageUrls }
+                        imageDescriptions={ item.imageDescriptions }
+                    />
+                )) }
+            </div>
+            {/* <ReferencesSection references={ references } /> */ }
+            {/* خط فاصل أسفل القسم */ }
+            <hr className="mt-10 border-t-2 border-dotted border-stone-300 w-1/2 mx-auto" />
+        </div>
     )
 }
 
-export default MilitaryWars
+export default MilitaryWars;
