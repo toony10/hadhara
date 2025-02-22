99ae228fcc6aca54a5a54fff39ae60fa1fc6e918
toony10 <amiranton257@gmail.com>
2025-02-23 00:42:18 +0200

    politics/king

--- app/commponents/politics/King.tsx
+++ app/commponents/politics/King.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import TopicHeader from '../custome/TopicHeader';
+import ContentBlock from '../custome/ContentBlock';
+// import ReferencesSection from '../custome/ReferencesSection';
+
+interface ContentData {
+    title: string;
+    description?: string;
+    listItems?: { title: string; description?: string }[];
+    imageUrls?: string[];
+    imageDescriptions?: string[];
+}
 
 function King() {
+    const [data, setData] = useState<ContentData[]>([]);
+
+
+    useEffect(() => {
+        fetch("/data/Politics/King.json")
+            .then((res) => res.json())
+            .then((jsonData) => setData(jsonData));
+    }, []);
+
     return (
-        <div>King</div>
+        <div>
+            <TopicHeader
+                title="الملك"
+                description=""
+            />
+            <div>
+                { data.map((item, index) => (
+                    <ContentBlock
+                        key={ index }
+                        title={ item.title }
+                        description={ item.description }
+                        listItems={ item.listItems }
+                        imageUrls={ item.imageUrls }
+                        imageDescriptions={ item.imageDescriptions }
+                    />
+                )) }
+            </div>
+            {/* <ReferencesSection references={ references } /> */ }
+            {/* خط فاصل أسفل القسم */ }
+            <hr className="mt-10 border-t-2 border-dotted border-stone-300 w-1/2 mx-auto" />
+        </div>
     )
 }
 
-export default King
+export default King;
