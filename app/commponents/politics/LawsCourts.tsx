8c3a4df02ab45869c253262416b25680a526a604
toony10 <amiranton257@gmail.com>
2025-02-24 01:22:27 +0200

    Laws

--- app/commponents/politics/LawsCourts.tsx
+++ app/commponents/politics/LawsCourts.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import TopicHeader from '../custome/TopicHeader';
+import ContentBlock from '../custome/ContentBlock';
+// import ReferencesSection from '../custome/ReferencesSection';
+
+interface ContentData {
+    title: string;
+    description?: string;
+    listItems?: { title: string; description?: string }[];
+    imageUrls?: string[];
+    imageDescriptions?: string[];
+}
 
 function LawsCourts() {
+    const [data, setData] = useState<ContentData[]>([]);
+
+
+    useEffect(() => {
+        fetch("/data/Politics/LawsCourts.json")
+            .then((res) => res.json())
+            .then((jsonData) => setData(jsonData));
+    }, []);
+
     return (
-        <div>LawsCourts</div>
+        <div>
+            <TopicHeader
+                title="القوانين والمحاكم"
+                description=""
+            />
+            <div>
+                { data.map((item, index) => (
+                    <ContentBlock
+                        key={ index }
+                        title={ item.title }
+                        description={ item.description }
+                        listItems={ item.listItems }
+                        imageUrls={ item.imageUrls }
+                        imageDescriptions={ item.imageDescriptions }
+                    />
+                )) }
+            </div>
+            {/* <ReferencesSection references={ references } /> */ }
+            {/* خط فاصل أسفل القسم */ }
+            <hr className="mt-10 border-t-2 border-dotted border-stone-300 w-1/2 mx-auto" />
+        </div>
     )
 }
 
-export default LawsCourts
+export default LawsCourts;
