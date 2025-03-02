bccb0cbcb3cdfc269b49d91e50cd9dfda5c05b6b
toony10 <amiranton257@gmail.com>
2025-03-02 21:35:36 +0200

    Clothes1

--- app/commponents/daily-life/Clothes.tsx
+++ app/commponents/daily-life/Clothes.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import TopicHeader from '../custome/TopicHeader';
+import ContentBlock from '../custome/ContentBlock';
+
+// import ReferencesSection from '../custome/ReferencesSection';
+
+interface ContentData {
+    title: string;
+    description?: string;
+    listItems?: { title: string; description?: string }[];
+    imageUrls?: string[];
+    imageDescriptions?: string[];
+}
 
 function Clothes() {
+    const [data, setData] = useState<ContentData[]>([]);
+
+    useEffect(() => {
+        fetch("/data/daily-life/Clothes.json")
+            .then((res) => res.json())
+            .then((jsonData) => setData(jsonData));
+    }, []);
+
     return (
-        <div>Clothes</div>
-    )
+        <div className="min-h-screen py-16">
+            <TopicHeader
+                title="الملابس والزينه في مصر القديمه"
+                description=""
+            />
+            <div>
+                { data.map((item, index) => (
+                    <ContentBlock
+                        key={ index }
+                        title={ item.title }
+                        description={ item.description }
+                        listItems={ item.listItems }
+                        imageUrls={ item.imageUrls }
+                        imageDescriptions={ item.imageDescriptions }
+                    />
+                )) }
+            </div>
+            {/* <ReferencesSection references={ references } /> */ }
+            {/* خط فاصل أسفل القسم */ }
+            <hr className="mt-10 border-t-2 border-dotted border-stone-300 w-1/2 mx-auto" />
+        </div>
+    );
 }
 
 export default Clothes;
