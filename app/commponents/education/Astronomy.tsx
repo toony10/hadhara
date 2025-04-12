6fba4e1d1b55428cd4fadedb600cb478cceb47a5
toony10 <amiranton257@gmail.com>
2025-04-12 17:40:57 +0200

    Astronomy

--- app/commponents/education/Astronomy.tsx
+++ app/commponents/education/Astronomy.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import TopicHeader from '../custome/TopicHeader';
+import ContentBlock from '../custome/ContentBlock';
+// import ReferencesSection from '../custome/ReferencesSection';
+
+interface ContentData {
+    title: string;
+    description?: string;
+    listItems?: { title: string; description?: string }[];
+    imageUrls?: string[];
+    imageDescriptions?: string[];
+}
 
 function Astronomy() {
+    const [data, setData] = useState<ContentData[]>([]);
+
+
+    useEffect(() => {
+        fetch("/data/education/Astronomy.json")
+            .then((res) => res.json())
+            .then((jsonData) => setData(jsonData));
+    }, []);
+
     return (
-        <div>Astronomy</div>
+        <div>
+            <TopicHeader
+                title="الفلك في مصر القديمة"
+                description="من تتبع مسارات الشمس والقمر إلى تقويمات دقيقة للزمن، شكّل الفلك عند المصريين القدماء علمًا مقدسًا يربط بين الخلود والكون والإله."
+            />
+            <div>
+                { data.map((item, index) => (
+                    <ContentBlock
+                        key={ index }
+                        title={ item.title }
+                        description={ item.description }
+                        listItems={ item.listItems }
+                        imageUrls={ item.imageUrls }
+                        imageDescriptions={ item.imageDescriptions }
+                    />
+                )) }
+            </div>
+            {/* <ReferencesSection references={ references } /> */ }
+            {/* خط فاصل أسفل القسم */ }
+
+        </div>
     )
 }
 
-export default Astronomy
+export default Astronomy;
