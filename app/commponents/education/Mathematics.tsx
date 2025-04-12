419595df15cd3b62536af660691e7a6758b25def
toony10 <amiranton257@gmail.com>
2025-04-12 17:50:09 +0200

    Mathematics

--- app/commponents/education/Mathematics.tsx
+++ app/commponents/education/Mathematics.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import TopicHeader from '../custome/TopicHeader';
+import ContentBlock from '../custome/ContentBlock';
+// import ReferencesSection from '../custome/ReferencesSection';
+
+interface ContentData {
+    title: string;
+    description?: string;
+    listItems?: { title: string; description?: string }[];
+    imageUrls?: string[];
+    imageDescriptions?: string[];
+}
 
 function Mathematics() {
+    const [data, setData] = useState<ContentData[]>([]);
+
+
+    useEffect(() => {
+        fetch("/data/education/Mathematics.json")
+            .then((res) => res.json())
+            .then((jsonData) => setData(jsonData));
+    }, []);
+
     return (
-        <div>Mathematics</div>
+        <div>
+            < TopicHeader
+                title="الرياضيات والهندسة في مصر القديمة"
+                description="طوّع المصري القديم الأرقام والهندسة لبناء أهرام خالدة، وقياس الأرض بعد الفيضان، وتنظيم ضرائب الدولة بدقة مذهلة، جاعلًا من الرياضيات علمًا للحياة والخلود."
+            />
+            <div>
+                { data.map((item, index) => (
+                    <ContentBlock
+                        key={ index }
+                        title={ item.title }
+                        description={ item.description }
+                        listItems={ item.listItems }
+                        imageUrls={ item.imageUrls }
+                        imageDescriptions={ item.imageDescriptions }
+                    />
+                )) }
+            </div>
+            {/* <ReferencesSection references={ references } /> */ }
+            {/* خط فاصل أسفل القسم */ }
+
+        </div>
     )
 }
 
-export default Mathematics
+export default Mathematics;
+
